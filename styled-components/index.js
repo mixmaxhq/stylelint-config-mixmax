d11533437fdce32579ee2ca1d69e1e58fa8b1fdc
Andy Tran <hello@andytran.me>
2019-05-21 10:27:17 -0700

    Added in styled-components config.

---
+++ styled-components/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  'processors': [
+    [
+      'stylelint-processor-styled-components',
+      {
+        'parserPlugins': [
+          'jsx',
+          'classProperties',
+          'exportDefaultFrom',
+        ],
+      },
+    ],
+  ],
+  'extends': [
+    '../index.js',
+    'stylelint-config-styled-components',
+  ],
+  'rules': {
+    'declaration-empty-line-before': null,
+    'value-keyword-case': null,
+  }
+};
